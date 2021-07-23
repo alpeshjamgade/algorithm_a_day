2bd9cce10fd63dcceb0bc3c4e055a9b003fd3a58
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2021-07-23 20:45:47 +0530

    added method2 in problem A

--- binary_search/problems/A/A.cpp
+++ binary_search/problems/A/A.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
+int upper_bound(vector<int> arr, int left, int right, int val)
+{
+    if (left < right)
+    {
+        int mid = left - (left - right) / 2;
+        int middle_elem = arr[mid];
+        if (middle_elem >= val)
+        {
+            return upper_bound(arr, mid + 1, right, val);
+        }
+        else
+        {
+            return upper_bound(arr, left, mid, val);
+        }
+    }
+    else
+    {
+        return left;
+    }
+}
+
+int solve2(vector<int> heights, int H)
+{
+    int at = upper_bound(heights, 0, heights.size() - 1, H);
+    return at + 1;
+}
+
 int solve(vector<int> heights, int H)
 {
     int at = distance(heights.begin(), upper_bound(heights.begin(), heights.end(), H, greater<int>()));
@@ -24,5 +51,6 @@ int main()
     cin >> H;
     // sort(heights.begin(), heights.end());
     cout << solve(heights, H) << '\n';
+    cout << solve2(heights, H) << '\n';
     return 0;
 }
