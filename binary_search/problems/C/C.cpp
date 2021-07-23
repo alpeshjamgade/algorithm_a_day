c52cf51bfa3ec46ded7b18ee145dfdcd7fa7a69e
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2021-07-23 20:46:38 +0530

    added method 2 in problem C in binary search

--- binary_search/problems/C/C.cpp
+++ binary_search/problems/C/C.cpp
@@ -2,6 +2,56 @@
 
 using namespace std;
 
+int upper_bound(vector<int> arr, int left, int right, int value)
+{
+    if (left < right)
+    {
+        int mid = left - (left - right) / 2;
+        int middle_elem = arr[mid];
+
+        if (middle_elem <= value)
+        {
+            return upper_bound(arr, mid + 1, right, value);
+        }
+        else
+        {
+            return upper_bound(arr, left, mid, value);
+        }
+    }
+    else
+    {
+        return left;
+    }
+}
+
+int lower_bound(vector<int> arr, int left, int right, int value)
+{
+    if (left < right)
+    {
+        int mid = left - (left - right) / 2;
+        int middle_elem = arr[mid];
+
+        if (middle_elem >= value)
+        {
+            return lower_bound(arr, left, mid, value);
+        }
+        else
+        {
+            return lower_bound(arr, mid + 1, right, value);
+        }
+    }
+    else
+    {
+        return right;
+    }
+}
+
+int solve(vector<int> arr, int value)
+{
+    int count = abs(lower_bound(arr, 0, arr.size() - 1, value) - upper_bound(arr, 0, arr.size() - 1, value));
+    return count;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
@@ -22,6 +72,7 @@ int main()
         cin >> x;
 
         cout << distance(lower_bound(arr.begin(), arr.end(), x), upper_bound(arr.begin(), arr.end(), x)) << '\n';
+        cout << solve(arr, x) << '\n';
     }
     return 0;
 }
