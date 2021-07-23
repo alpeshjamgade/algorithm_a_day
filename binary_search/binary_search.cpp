3d3d798266738466e9b01da74eb45f10d2f90e8b
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2021-07-23 20:31:19 +0530

    added binary search algorithm

---
+++ binary_search/binary_search.cpp
@@ -0,0 +1,50 @@
+/**
+ *    author:  Alpesh Jamgade
+ *    created:  19.07.2021 21:35:16 
+ *    run: g++ -std=c++17 -ulimit -Wall -Wno-unused-result -pthread -O2 binary_search.cpp -o binary_search && ./binary_search < sample1.in   
+**/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int binary_search(vector<int> arr, int left, int right, int val)
+{
+    if (left < right)
+    {
+        int mid = left - (left - right) / 2;
+        int middle_elem = arr[mid];
+        if (middle_elem == val)
+        {
+            return mid;
+        }
+        else if (middle_elem < val)
+        {
+            return binary_search(arr, mid + 1, right, val);
+        }
+        else
+        {
+            return binary_search(arr, left, mid - 1, val);
+        }
+    }
+    else
+    {
+        return left;
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+    cout << binary_search(arr, 0, n - 1, m) << endl;
+
+    return 0;
+}
