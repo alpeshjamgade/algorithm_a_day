88f97108d2043e69a22ba519c701cb207d065a8f
Alpesh Jamgade <alpeshjamgade21@gmail.com>
2021-07-23 20:47:05 +0530

    added problem D in binary search

---
+++ binary_search/problems/D/D.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define lcm(a, b) ((a * b) / __gcd(a, b))
+
+using namespace std;
+ofstream fout("out");
+
+int N, a, b;
+
+int count_terms(int value)
+{
+    return value / a + value / b - value / lcm(a, b);
+}
+
+int upper_bound(int left, int right)
+{
+    if (left < right)
+    {
+        int mid = left - ((left - right) / 2);
+        int terms = count_terms(mid);
+
+        if (terms < N)
+        {
+            return upper_bound(mid + 1, right);
+        }
+        else
+        {
+            return upper_bound(left, mid);
+        }
+    }
+    else
+    {
+        return left;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> a >> b >> N;
+    cout << upper_bound(1, 1000000000) << '\n';
+    return 0;
+}
